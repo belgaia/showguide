831899423beb9b745d4d0f6de0b4767c9835ebad
Isabel Batista <isabelbatista@gmx.de>
2014-07-08 20:51:59 +0200

    Fixed encoding problem with workaround.

--- showguide_showresult.py
+++ showguide_showresult.py
@@ -1,6 +1,7 @@
 #!/Python34/python
 import cgi
 import mongodbConnector
+import os
 
 print("Content-type: text/html; charset=utf8")
 print()
@@ -15,15 +16,31 @@ print()
 print("<h2>Episodeguide</h2>")
 print()
 
+# this is just a workaround -- fix problems with encoding
+def replaceUmlaute(content):
+    fixedContent = content.replace('ü', 'ue')
+    fixedContent = fixedContent.replace('ö', 'oe')
+    fixedContent = fixedContent.replace('ä', 'ae')
+    fixedContent = fixedContent.replace('ß', 'ss')
+
+    return fixedContent
+
 documents = mongodbConnector.getEpisodeguide(series)
 for doc in documents:
     print("<p>")
     print("<b>" + doc["dvd_number"]
           + " - " + doc["episode_number"]
           + " - " + doc["episode_name_de"] + "</b><br>")
-    print(doc["content"])
+    content = replaceUmlaute(doc["content"])
+    print(content)
     print("</p>")
     print()
+
+
+
+
+
+
 
 
 

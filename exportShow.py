6531913189fab75085d70e8d551c3bf264f73310
Isabel Batista <isabelbatista@gmx.de>
2014-07-27 22:56:03 +0200

    A bit formatting of the export page.

--- exportShow.py
+++ exportShow.py
@@ -9,19 +9,19 @@ print("<form action='exportShowResult.py' method='post'>")
 
 print("<h1>Export</h1>")
 print("<p>")
-print("<p>Mit dem nachfolgenden Formular kann eine Serie oder mehrere Serien ausgewaehlt werden,"
+print("<p>Mit dem nachfolgenden Formular kann eine Serie oder mehrere Serien ausgewaehlt werden, "
       "um den bisher gespeicherten Inhalt in eine CSV-Datei zu exportieren.</p>")
 
 shows = mongodbConnector.getAllSeries()
 
 print("<table width='50%'><tr>")
-print("<td>Serie:")
+print("<td valign='top'>Serie:</td><td>")
 print("<select multiple name='series_choice' size='10'>")
 print("<option selected>Alle</option>")
 for showname in shows:
 	print("<option>" + showname + "</option>")
 print("</select></td></tr>")
-print("<tr><td>Zielverzeichnis: <input name='targetDir' type='text'></td></tr></table>")
+print("<tr><td>Zielverzeichnis:</td><td><input name='targetDir' type='text'></td></tr></table>")
 
 print("<input type='submit' value='Export Show'/>")
 print("</form>")

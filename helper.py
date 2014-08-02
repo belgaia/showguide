d84c19858f0be3ac9c9ff0c4402f055be28970f7
Isabel Batista <isabelbatista@gmx.de>
2014-08-02 19:22:13 +0200

    Added upper case umlaute to the view converter.

--- helper.py
+++ helper.py
@@ -3,8 +3,14 @@ __author__ = 'Agile Developers'
 # this is just a workaround -- fix problems with encoding
 def replaceUmlaute(content):
     fixedContent = content.replace('ü', 'ue')
+    fixedContent = fixedContent.replace('Ü', 'Ue')
+
     fixedContent = fixedContent.replace('ö', 'oe')
+    fixedContent = fixedContent.replace('Ö', 'Oe')
+
     fixedContent = fixedContent.replace('ä', 'ae')
+    fixedContent = fixedContent.replace('Ä', 'ae')
+
     fixedContent = fixedContent.replace('ß', 'ss')
 
     return fixedContent
